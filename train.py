4d7d231278943dec9b562412c9d8d4528f48001f
Oscar Saharoy <osaharoy@gmail.com>
2024-05-15 08:06:42 +0100

    learns i am

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ t is the input token
 e_t is the embedding of the input token
 e_c_prev is the embedding of the previous context
 e_c is the embedding of the new context
+tt is the target token
 
 e_t = f(t)
 one hot to token embedding
@@ -25,11 +26,36 @@ context embedding to one hot ish
 
 """
 
-input_size = 100
+# load dataset
+
+with open( "wordmap.json", mode="r" ) as f:
+    wordmap = json.load( f )
+
+mapword = { v: k for k, v in wordmap.items() }
+vocab_size = len(wordmap)
+x = []
+
+with open( "sentences.txt", 'r' ) as f:
+    for line in f:
+        words = [
+            wordmap[word]
+            for word in re.findall(r"[\w']+|[.,!?;]", line)
+            if word and word in wordmap and wordmap[word] < vocab_size
+        ] + [ 0 ]
+        x.extend( [ ([0] + words[:i], elm) for i, elm in enumerate(words) ] )
+
+# train and test set
+
+training_data = x[:len(x) // 10 * 9]
+test_data = x[len(x) // 10 * 9:]
+
+input_size = 14
 embedding_size = 10
 
+# funcs
+
 def onehot( x ):
-    res = np.zeros(embedding_size)
+    res = np.zeros(input_size)
     res[x] = 1
     return res
 
@@ -49,41 +75,59 @@ def predict( t, e_c_prev, weights ):
     pred = softmax( w2 @ e_c )
     return pred
 
-def loss( t, e_c_prev, weights, target ):
-    pureloss = predict( t, e_c_prev, weights ) - target
+def loss( t, e_c_prev, weights, tt ):
+    pureloss = predict( t, e_c_prev, weights ) - tt
     return pureloss.T @ pureloss
 
-def check( t, e_c_prev, weights, target ):
-    return np.argmax( predict( t, e_c_prev, weights ) ) == np.argmax( target )
+def check( t, e_c_prev, weights, tt ):
+    return np.argmax( predict( t, e_c_prev, weights ) ) == np.argmax( tt )
 
-def test_eval( t, e_c_prev, weights, targets ):
-    return sum(
-        check( t, e_c_prev, weights, target )
-        for t, target in zip(obss, targets)
-    ) / targets.shape[0]
+def test_eval( test_data, weights ):
+    res = 0
+    for past_tokens, next_token in test_data:
+        e_c_prev = np.zeros(embedding_size)
+        t = onehot(past_tokens[-1])
+        tt = onehot(next_token)
+        res += check( t, e_c_prev, weights, tt )
+    return res / len( test_data )
 
-def dldw( t, e_c_prev, weights, target ):
+def dldw( t, e_c_prev, weights, tt ):
     w1, w2, w3 = weights
 
     z_e_t = w1 @ t
     e_t = act( z_e_t )
-    z_e_c = w2 @ np.concatenate( e_t, e_c_prev )
+    z_e_c = w2 @ np.concatenate(( e_t, e_c_prev ))
     e_c = act( z_e_c )
     z_pred = w3 @ e_c
     pred = softmax( z_pred )
 
-    error_pred = 2 * ( pred - target ) * pred * ( 1 - pred )
+    error_pred = 2 * ( pred - tt ) * pred * ( 1 - pred )
     error_e_c = w3.T @ error_pred * dact(z_e_c)
-    error_e_t = w2.T @ error_e_c * dact(z_e_t)
+    error_e_t = ( w2.T @ error_e_c )[:embedding_size] * dact(z_e_t)
 
     dlossdw3 = np.outer( error_pred, e_c )
-    dlossdw2 = np.outer( error_e_c, e_t )
+    dlossdw2 = np.outer( error_e_c, np.concatenate(( e_t, e_c_prev )) )
     dlossdw1 = np.outer( error_e_t, t )
 
     return dlossdw1, dlossdw2, dlossdw3
 
+def write( weights ):
+    sentence = []
+    e_c_prev = np.zeros(embedding_size)
+    t = onehot(0)
+
+    while (len(sentence) == 0 or sentence[-1] != "") and len(sentence) < 100:
+        pred = predict( t, e_c_prev, weights )
+        word = mapword[ np.argmax(pred) ]
+        sentence.append(word)
+        t = onehot( np.argmax(pred) )
+
+    print(sentence)
+
+# init network
+
 np.random.seed(1)
-a = 0.005
+a = 0.05
 scale = .2
 
 w0 = np.random.rand( embedding_size, input_size ) - .5
@@ -94,46 +138,28 @@ w1 *= scale
 w2 *= scale
 weights = [ w0, w1, w2 ]
 
-# load dataset
-
-with open( "wordmap.json", mode="r" ) as f:
-    wordmap = json.load( f )
-
-vocab_size = len(wordmap)
-x = []
-
-with open( "sentences.txt", 'r' ) as f:
-    for line in f:
-        words = [
-            wordmap[word]
-            for word in re.findall(r"[\w']+|[.,!?;]", line)
-            if word and word in wordmap and wordmap[word] < vocab_size
-        ] + [ 0 ]
-        x.extend( [ (words[:i], elm) for i, elm in enumerate(words) ] )        
-
-# train and test set
-
-training_data = x[:len(x) // 10 * 9]
-test_data = x[len(x) // 10 * 9:]
-
 # training loop
 
 try:
-    for epoch in range(11):
+    for epoch in range(110):
         for past_tokens, next_token in training_data:
-            e_c_prev = np.zeros(embedding_size)            
+            e_c_prev = np.zeros(embedding_size)
             t = onehot(past_tokens[-1])
+            tt = onehot(next_token)
 
-            dw0, dw1, dw2 = dldw( obs, weights, target )
+            dw0, dw1, dw2 = dldw( t, e_c_prev, weights, tt )
             weights[0] -= a * dw0
             weights[1] -= a * dw1
+            weights[2] -= a * dw2
         print(
             "epoch", epoch,
-            "- test accuracy", f"{test_eval( obss_test, weights, targets_test ) * 100:.1f}%"
+            "- test accuracy", f"{test_eval( test_data, weights ) * 100:.1f}%"
         )
 
 except KeyboardInterrupt:
     print("ending training!")
+
+write( weights )
 
 # save the weights to a file
 
